2797f83fcf0795989429b81d4991fe67c07739f3
Yogavelan-BJ <yogavelanbj@gmail.com>
2024-10-24 23:47:45 +0530

    events and view events reusable component

---
+++ src/data/viewEventsData.js
@@ -0,0 +1,76 @@
+import cyberconclave from "../assets/cyberConclave.png";
+import ctf from "../assets/ctf.png";
+export const data = {
+  cyberconclave: {
+    title: "Cyber Conclave",
+    title_font: "font-orbitron",
+    title_size: "text-3xl",
+    description:
+      "Dive into the Details of leading experts converging in a panel discussion",
+
+    brief:
+      "At the Cyber Conclave, leading experts converge in a dynamic panel discussion, offering a comprehensive dive into the intricate details of cybersecurity. This exclusive event brings together thought leaders, industry pioneers, and tech innovators to explore emerging trends, best practices, and pressing challenges facing the digital landscape.",
+    brief2:
+      "In today’s digital age, the proliferation of online scams and cyber threats poses a significant risk to individuals and organizations alike. The alarming rise in these incidents has sparked a pressing need for robust discussions and actionable strategies among professionals across various sectors. To address these challenges, we invite you to participate in the upcoming",
+    poster: cyberconclave,
+    url: "cyberconclave",
+    speakers: [
+      ["Name", "Designation"],
+      ["Name", "Designation"],
+      ["Name", "Designation"],
+    ],
+    eventDetails: {
+      time: "8AM - 9AM",
+      date: "21 FEB 2025",
+      fees: "200 per team",
+      seats: "150 SEATS",
+    },
+    discussion: ["Topic 1", "Topic 2", "Topic 3"],
+  },
+
+  capturetheflag: {
+    title: "Capture The Flag",
+    title_font: "font-saira",
+    title_size: "text-4xl",
+    description: "Embark on a quest to seize the victory flags!",
+
+    brief:
+      "The Capture the Flag (CTF) Event is back, offering participants an intense, fast-paced competition designed to test your problem-solving, coding, and cybersecurity expertise. Whether you're a beginner or an experienced hacker, this event is the perfect opportunity to dive into real-world challenges and show off your skills!",
+    brief2:
+      "The Capture the Flag (CTF) Event is back, offering participants an intense, fast-paced competition designed to test your problem-solving, coding, and cybersecurity expertise. Whether you're a beginner or an experienced hacker, this event is the perfect opportunity to dive into real-world challenges and show off your skills!",
+    poster: ctf,
+    url: "capturetheflag",
+    facultyCoordinator: ["faculty name 1", "faculty name 2"],
+    studentCoordinator: ["student name 1", "student name 2"],
+    eventDetails: {
+      time: "10AM - 11AM",
+      date: "22 FEB 2025",
+      fees: "250 per team",
+      seats: "150 SEATS",
+    },
+    rules: ["rule 1", "rule 2", "rule 3"],
+  },
+
+  cyberthon: {
+    title: "Cyberthon",
+    title_font: "font-saira",
+    title_size: "text-4xl",
+    description: "Embark on a quest to seize the victory flags!",
+
+    brief:
+      "The Capture the Flag (CTF) Event is back, offering participants an intense, fast-paced competition designed to test your problem-solving, coding, and cybersecurity expertise. Whether you're a beginner or an experienced hacker, this event is the perfect opportunity to dive into real-world challenges and show off your skills!",
+    brief2:
+      "The Capture the Flag (CTF) Event is back, offering participants an intense, fast-paced competition designed to test your problem-solving, coding, and cybersecurity expertise. Whether you're a beginner or an experienced hacker, this event is the perfect opportunity to dive into real-world challenges and show off your skills!",
+    poster: ctf,
+    url: "cyberthon",
+    facultyCoordinator: ["faculty name 1", "faculty name 2"],
+    studentCoordinator: ["student name 1", "student name 2"],
+    eventDetails: {
+      time: "10AM - 11AM",
+      date: "22 FEB 2025",
+      fees: "250 per team",
+      seats: "150 SEATS",
+    },
+    rules: ["rule 1", "rule 2", "rule 3"],
+  },
+};
