f7759ede34502f0c67491cc11e84b99dfcb43fb8
swapna-7 <swapnamuralikrishna07042005@gmail.com>
2024-10-29 10:12:01 +0530

    about us pics

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion, useInView } from 'framer-motion';
 import { useRef } from 'react';
 import srmbuild from '../../assets/srmbuild.jpg';
-import cyberCarnivalPng from '../../assets/cyberCarnivalPng.png';
+import cyberCarnivalPng from '../../assets/bg_logo.png';
 import computerCyber from '../../assets/computerCyber.png';
 import Background from '../AboutUs/Aboutbg'; 
 import BlobBubble from '../AboutUs/AboutBlob'; 
@@ -38,7 +38,7 @@ const About = () => {
               <div className="hidden lg:block absolute top-6 transform rotate-45 w-36 h-1 bg-teal-300/80 shadow-glow"></div>
             </div>
 
-            <img src={imageSrc} alt={title} className="w-3/4 h-auto justify-self-center" />
+            <img src={imageSrc} alt={title} className={` ${title=="Cyber Carnival"?"w-1/2":title=='SRMIST'?"w-1/2":"w-3/4"}   h-auto justify-self-center`} />
 
             {/* Bottom Strip Lights */}
             <div className="absolute -bottom-20 left-0">
@@ -72,7 +72,7 @@ const About = () => {
           "The Cyber Carnival at SRM Ramapuram is an electrifying event hosted by the cybersecurity department, promising an immersive journey into the world of digital defense and innovation. Fusing education with excitement, the carnival showcases cutting-edge technologies, interactive workshops, and engaging competitions designed to sharpen skills and raise awareness about cybersecurity threats and solutions. Participants can expect riveting demonstrations, insightful talks by industry experts, and hands-on experiences that illuminate the importance of safeguarding our digital landscape.")
         }
         
-        {createSection(cyberCarnivalPng, "SRMIST", 
+        {createSection(srmbuild, "SRMIST", 
           "SRM Institute of Science and Technology, Ramapuram campus, is a premier educational institution located in Chennai, Tamil Nadu. Known for its robust academic programs, the campus offers a diverse range of undergraduate, postgraduate, and doctoral courses in engineering, management, science, and humanities. The Ramapuram campus is equipped with state-of-the-art infrastructure, modern laboratories, and a well-stocked library, providing students with an enriching learning environment.")
         }
         
